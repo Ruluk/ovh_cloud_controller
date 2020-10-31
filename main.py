8858a2f12f87aa0aa7e5b6b3b038338af5ca2fe6
Juan Carlos <ruluk@users.noreply.github.com>
2020-10-30 19:30:34 -0500

    Refactor: Reorder methods in main.py
    Signed-off-by: Juan Carlos <ruluk@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,23 +6,6 @@ region = "BHS5"
 instance_model = "b2-15"
 
 
-def get_optimal_flavor_id() -> str:
-    flavors = provider.get_flavors(project_id)
-    filtered_flavors = [flavor["id"] for flavor in flavors if
-                        flavor["region"] == region and flavor["name"] == instance_model]
-    return filtered_flavors[0]
-
-
-def get_latest_backup(prefix: str):
-    backups = provider.get_backups(project_id)
-    filtered_backups = [backup for backup in backups if backup["name"].startswith(prefix)]
-    return filtered_backups[0]
-
-
-def get_ssh_key() -> str:
-    return provider.get_ssh_keys(project_id)[0]["id"]
-
-
 def create_instance_from_backup(backup: dict, flavor_id: str, ssh_key: str):
     provider.create_instance(project_id, {
         "flavorId": flavor_id,
@@ -33,9 +16,29 @@ def create_instance_from_backup(backup: dict, flavor_id: str, ssh_key: str):
     })
 
 
-if __name__ == '__main__':
+def get_latest_backup(prefix: str):
+    backups = provider.get_backups(project_id)
+    filtered_backups = [backup for backup in backups if backup["name"].startswith(prefix)]
+    return filtered_backups[0]
+
+
+def get_optimal_flavor_id() -> str:
+    flavors = provider.get_flavors(project_id)
+    filtered_flavors = [flavor["id"] for flavor in flavors if
+                        flavor["region"] == region and flavor["name"] == instance_model]
+    return filtered_flavors[0]
+
+
+def get_ssh_key() -> str:
+    return provider.get_ssh_keys(project_id)[0]["id"]
+
+
+def main():
     flavor_id = get_optimal_flavor_id()
     ssh_key = get_ssh_key()
     backup = get_latest_backup(backup_prefix)
-
     create_instance_from_backup(backup, flavor_id, ssh_key)
+
+
+if __name__ == '__main__':
+    main()
