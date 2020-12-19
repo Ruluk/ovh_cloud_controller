086b72a224798f0ff1c3700bb75483a52e2221fd
Juan Carlos <ruluk@users.noreply.github.com>
2020-12-19 12:57:12 -0500

    Feat: Run CLI in main.
    Signed-off-by: Juan Carlos <ruluk@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from src.cloud_controllers import instance
-
-project_id = "cf542abba8324af5b07aa54d2b91fa31"
-backup_prefix = "big-blue"
-region = "BHS5"
-instance_model = "b2-15"
+from src import cli
 
 
 def main():
-    _, ip = instance.create_from_latest_backup(project_id, region, instance_model, backup_prefix)
-    print(ip)
+    cli.run()
 
 
 if __name__ == '__main__':
     main()
-    # delete_all_instances()
